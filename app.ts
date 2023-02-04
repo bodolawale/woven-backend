016fc5a9d8d2c5ceec9c93ea9c6c382d3678876e
bodolawale <bodolawale@gmail.com>
2023-02-04 23:47:19 +0100

    project init

---
+++ app.ts
@@ -0,0 +1,29 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import ProfileRoutes from './src/profile/route';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/profile', ProfileRoutes);
+
+function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
+  res.status(500).send({
+    message: 'An error occurred while processing this request',
+    data: err.toString()
+  });
+}
+
+function error404(req: Request, res: Response, next: NextFunction) {
+  res.status(404).send({
+    success: false,
+    message: 'Invalid URL'
+  });
+}
+
+app.use(errorHandler);
+app.use(error404);
+
+export default app;
---
+++ main.ts
@@ -0,0 +1,10 @@
+import app from './app';
+
+const processPort = process.env.PORT || '3000';
+const PORT = parseInt(processPort, 0);
+
+const ENV = process.env.NODE_ENV || 'development';
+
+app.listen(PORT, () => {
+  console.log(`App running in ${ENV} mode on port ${PORT}`);
+});
